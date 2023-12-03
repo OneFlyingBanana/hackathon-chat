0ffd2cb68147c8ce9cc56a236d58b4c0ef4eb31d
joanna <j.baranowska@hotmail.com>
2023-12-03 15:23:04 +0100

    template chat

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,35 @@
-import { Web5 } from "@web5/api";
-import { useState, useEffect } from "react";
-import { NoChatSelected } from "@/components/NoChatSelected";
-import { Sidebar } from "@/components/Sidebar";
-import { Chat } from "@/components/Chat";
+import {
+  Web5
+} from "@web5/api";
+import {
+  useState,
+  useEffect
+} from "react";
+import {
+  NoChatSelected
+} from "@/components/NoChatSelected";
+import {
+  Sidebar
+} from "@/components/Sidebar";
+import {
+  Chat
+} from "@/components/Chat";
+import React from "react";
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBIcon,
+  MDBBtn,
+  MDBTypography,
+  MDBTextArea,
+  MDBCardHeader,
+} from "mdb-react-ui-kit";
 
 export default function Home() {
 
@@ -35,7 +62,10 @@ export default function Home() {
 
   useEffect(() => {
     const initWeb5 = async () => {
-      const { web5, did } = await Web5.connect();
+      const {
+        web5,
+        did
+      } = await Web5.connect();
       setWeb5(web5);
       setMyDid(did);
 
@@ -68,26 +98,41 @@ export default function Home() {
       },
       structure: {
         ding: {
-          $actions: [
-            { who: "anyone", can: "write" },
-            { who: "author", of: "ding", can: "read" },
-            { who: "recipient", of: "ding", can: "read" },
+          $actions: [{
+              who: "anyone",
+              can: "write"
+            },
+            {
+              who: "author",
+              of: "ding",
+              can: "read"
+            },
+            {
+              who: "recipient",
+              of: "ding",
+              can: "read"
+            },
           ],
         },
       },
     };
 
-    const { protocols, status: protocolStatus } =
-      await web5.dwn.protocols.query({
-        message: {
-          filter: {
-            protocol: "https://blackgirlbytes.dev/dinger-chat-protocol",
-          },
+    const {
+      protocols,
+      status: protocolStatus
+    } =
+    await web5.dwn.protocols.query({
+      message: {
+        filter: {
+          protocol: "https://blackgirlbytes.dev/dinger-chat-protocol",
         },
-      });
+      },
+    });
 
     if (protocolStatus.code !== 200 || protocols.length === 0) {
-      const { protocolStatus } = await web5.dwn.protocols.configure({
+      const {
+        protocolStatus
+      } = await web5.dwn.protocols.configure({
         message: {
           definition: dingerProtocolDefinition,
         },
@@ -109,7 +154,9 @@ export default function Home() {
   };
 
   const writeToDwn = async (ding) => {
-    const { record } = await web5.dwn.records.write({
+    const {
+      record
+    } = await web5.dwn.records.write({
       data: ding,
       message: {
         protocol: "https://blackgirlbytes.dev/dinger-chat-protocol",
@@ -135,7 +182,9 @@ export default function Home() {
 
     const ding = constructDing();
     const record = await writeToDwn(ding);
-    const { status } = await sendRecord(record);
+    const {
+      status
+    } = await sendRecord(record);
 
     console.log("Send record status", status);
 
@@ -158,7 +207,10 @@ export default function Home() {
   };
 
   const fetchDings = async (web5, did) => {
-    const { records, status: recordStatus } = await web5.dwn.records.query({
+    const {
+      records,
+      status: recordStatus
+    } = await web5.dwn.records.query({
       message: {
         filter: {
           protocol: "https://blackgirlbytes.dev/dinger-chat-protocol",
@@ -174,8 +226,8 @@ export default function Home() {
       );
 
       if (recordStatus.code == 200) {
-        const received = results.filter((result) => result?.recipient === did);
-        const sent = results.filter((result) => result?.sender === did);
+        const received = results.filter((result) => result ? .recipient === did);
+        const sent = results.filter((result) => result ? .sender === did);
         setReceivedDings(received);
         setSentDings(sent);
       }
@@ -204,41 +256,342 @@ export default function Home() {
     }
   };
 
-  return (
-    <div className="app-container">
-      <header>
-        <h1>Dinger</h1>
-      </header>
-      <main>
-        <Sidebar
-          groupedDings={groupedDings}
-          activeRecipient={activeRecipient}
-          handleSetActiveRecipient={handleSetActiveRecipient}
-          handleCopyDid={handleCopyDid}
-          handleStartNewChat={handleStartNewChat}
-          showNewChatInput={showNewChatInput}
-          didCopied={didCopied}
-          handleConfirmNewChat={handleConfirmNewChat}
-          setRecipientDid={setRecipientDid}
-          recipientDid={recipientDid}
-        />
-        <section>
-          {activeRecipient ? (
-            <Chat
-              activeRecipient={activeRecipient}
-              sortedDings={sortedDings}
-              myDid={myDid}
-              handleSubmit={handleSubmit}
-              noteValue={noteValue}
-              setNoteValue={setNoteValue}
-              errorMessage={errorMessage}
-              setErrorMessage={setErrorMessage}
-            />
-          ) : (
-            <NoChatSelected />
-          )}
-        </section>
-      </main>
-    </div>
+  return ( <
+    MDBContainer fluid className = "py-5"
+    style = {
+      {
+        backgroundColor: "#eee"
+      }
+    } >
+    <
+    MDBRow >
+    <
+    MDBCol md = "6"
+    lg = "5"
+    xl = "4"
+    className = "mb-4 mb-md-0" >
+    <
+    h5 className = "font-weight-bold mb-3 text-center text-lg-start" >
+    Member <
+    /h5>
+
+    <
+    MDBCard >
+    <
+    MDBCardBody >
+    <
+    MDBTypography listUnStyled className = "mb-0" >
+    <
+    li className = "p-2 border-bottom"
+    style = {
+      {
+        backgroundColor: "#eee"
+      }
+    } >
+    <
+    a href = "#!"
+    className = "d-flex justify-content-between" >
+    <
+    div className = "d-flex flex-row" >
+    <
+    img src = "https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-8.webp"
+    alt = "avatar"
+    className = "rounded-circle d-flex align-self-center me-3 shadow-1-strong"
+    width = "60" /
+    >
+    <
+    div className = "pt-1" >
+    <
+    p className = "fw-bold mb-0" > John Doe < /p> <
+    p className = "small text-muted" >
+    Hello, Are you there ?
+    <
+    /p> < /
+    div > <
+    /div> <
+    div className = "pt-1" >
+    <
+    p className = "small text-muted mb-1" > Just now < /p> <
+    span className = "badge bg-danger float-end" > 1 < /span> < /
+    div > <
+    /a> < /
+    li > <
+    li className = "p-2 border-bottom" >
+    <
+    a href = "#!"
+    className = "d-flex justify-content-between" >
+    <
+    div className = "d-flex flex-row" >
+    <
+    img src = "https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-1.webp"
+    alt = "avatar"
+    className = "rounded-circle d-flex align-self-center me-3 shadow-1-strong"
+    width = "60" /
+    >
+    <
+    div className = "pt-1" >
+    <
+    p className = "fw-bold mb-0" > Danny Smith < /p> <
+    p className = "small text-muted" >
+    Lorem ipsum dolor sit. <
+    /p> < /
+    div > <
+    /div> <
+    div className = "pt-1" >
+    <
+    p className = "small text-muted mb-1" > 5 mins ago < /p> < /
+    div > <
+    /a> < /
+    li > <
+    li className = "p-2 border-bottom" >
+    <
+    a href = "#!"
+    className = "d-flex justify-content-between" >
+    <
+    div className = "d-flex flex-row" >
+    <
+    img src = "https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-2.webp"
+    alt = "avatar"
+    className = "rounded-circle d-flex align-self-center me-3 shadow-1-strong"
+    width = "60" /
+    >
+    <
+    div className = "pt-1" >
+    <
+    p className = "fw-bold mb-0" > Alex Steward < /p> <
+    p className = "small text-muted" >
+    Lorem ipsum dolor sit. <
+    /p> < /
+    div > <
+    /div> <
+    div className = "pt-1" >
+    <
+    p className = "small text-muted mb-1" > Yesterday < /p> < /
+    div > <
+    /a> < /
+    li > <
+    li className = "p-2 border-bottom" >
+    <
+    a href = "#!"
+    className = "d-flex justify-content-between" >
+    <
+    div className = "d-flex flex-row" >
+    <
+    img src = "https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-3.webp"
+    alt = "avatar"
+    className = "rounded-circle d-flex align-self-center me-3 shadow-1-strong"
+    width = "60" /
+    >
+    <
+    div className = "pt-1" >
+    <
+    p className = "fw-bold mb-0" > Ashley Olsen < /p> <
+    p className = "small text-muted" >
+    Lorem ipsum dolor sit. <
+    /p> < /
+    div > <
+    /div> <
+    div className = "pt-1" >
+    <
+    p className = "small text-muted mb-1" > Yesterday < /p> < /
+    div > <
+    /a> < /
+    li > <
+    li className = "p-2 border-bottom" >
+    <
+    a href = "#!"
+    className = "d-flex justify-content-between" >
+    <
+    div className = "d-flex flex-row" >
+    <
+    img src = "https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-4.webp"
+    alt = "avatar"
+    className = "rounded-circle d-flex align-self-center me-3 shadow-1-strong"
+    width = "60" /
+    >
+    <
+    div className = "pt-1" >
+    <
+    p className = "fw-bold mb-0" > Kate Moss < /p> <
+    p className = "small text-muted" >
+    Lorem ipsum dolor sit. <
+    /p> < /
+    div > <
+    /div> <
+    div className = "pt-1" >
+    <
+    p className = "small text-muted mb-1" > Yesterday < /p> < /
+    div > <
+    /a> < /
+    li > <
+    li className = "p-2 border-bottom" >
+    <
+    a href = "#!"
+    className = "d-flex justify-content-between" >
+    <
+    div className = "d-flex flex-row" >
+    <
+    img src = "https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-5.webp"
+    alt = "avatar"
+    className = "rounded-circle d-flex align-self-center me-3 shadow-1-strong"
+    width = "60" /
+    >
+    <
+    div className = "pt-1" >
+    <
+    p className = "fw-bold mb-0" > Lara Croft < /p> <
+    p className = "small text-muted" >
+    Lorem ipsum dolor sit. <
+    /p> < /
+    div > <
+    /div> <
+    div className = "pt-1" >
+    <
+    p className = "small text-muted mb-1" > Yesterday < /p> < /
+    div > <
+    /a> < /
+    li > <
+    li className = "p-2" >
+    <
+    a href = "#!"
+    className = "d-flex justify-content-between" >
+    <
+    div className = "d-flex flex-row" >
+    <
+    img src = "https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp"
+    alt = "avatar"
+    className = "rounded-circle d-flex align-self-center me-3 shadow-1-strong"
+    width = "60" /
+    >
+    <
+    div className = "pt-1" >
+    <
+    p className = "fw-bold mb-0" > Brad Pitt < /p> <
+    p className = "small text-muted" >
+    Lorem ipsum dolor sit. <
+    /p> < /
+    div > <
+    /div> <
+    div className = "pt-1" >
+    <
+    p className = "small text-muted mb-1" > 5 mins ago < /p> <
+    span className = "text-muted float-end" >
+    <
+    MDBIcon fas icon = "check" / >
+    <
+    /span> < /
+    div > <
+    /a> < /
+    li > <
+    /MDBTypography> < /
+    MDBCardBody > <
+    /MDBCard> < /
+    MDBCol >
+
+    <
+    MDBCol md = "6"
+    lg = "7"
+    xl = "8" >
+    <
+    MDBTypography listUnStyled >
+    <
+    li className = "d-flex justify-content-between mb-4" >
+    <
+    img src = "https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp"
+    alt = "avatar"
+    className = "rounded-circle d-flex align-self-start me-3 shadow-1-strong"
+    width = "60" /
+    >
+    <
+    MDBCard >
+    <
+    MDBCardHeader className = "d-flex justify-content-between p-3" >
+    <
+    p className = "fw-bold mb-0" > Brad Pitt < /p> <
+    p className = "text-muted small mb-0" >
+    <
+    MDBIcon far icon = "clock" / > 12 mins ago <
+    /p> < /
+    MDBCardHeader > <
+    MDBCardBody >
+    <
+    p className = "mb-0" >
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. <
+    /p> < /
+    MDBCardBody > <
+    /MDBCard> < /
+    li > <
+    li class = "d-flex justify-content-between mb-4" >
+    <
+    MDBCard className = "w-100" >
+    <
+    MDBCardHeader className = "d-flex justify-content-between p-3" >
+    <
+    p class = "fw-bold mb-0" > Lara Croft < /p> <
+    p class = "text-muted small mb-0" >
+    <
+    MDBIcon far icon = "clock" / > 13 mins ago <
+    /p> < /
+    MDBCardHeader > <
+    MDBCardBody >
+    <
+    p className = "mb-0" >
+    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium. <
+    /p> < /
+    MDBCardBody > <
+    /MDBCard> <
+    img src = "https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-5.webp"
+    alt = "avatar"
+    className = "rounded-circle d-flex align-self-start ms-3 shadow-1-strong"
+    width = "60" /
+    >
+    <
+    /li> <
+    li className = "d-flex justify-content-between mb-4" >
+    <
+    img src = "https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp"
+    alt = "avatar"
+    className = "rounded-circle d-flex align-self-start me-3 shadow-1-strong"
+    width = "60" /
+    >
+    <
+    MDBCard >
+    <
+    MDBCardHeader className = "d-flex justify-content-between p-3" >
+    <
+    p className = "fw-bold mb-0" > Brad Pitt < /p> <
+    p className = "text-muted small mb-0" >
+    <
+    MDBIcon far icon = "clock" / > 10 mins ago <
+    /p> < /
+    MDBCardHeader > <
+    MDBCardBody >
+    <
+    p className = "mb-0" >
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. <
+    /p> < /
+    MDBCardBody > <
+    /MDBCard> < /
+    li > <
+    li className = "bg-white mb-3" >
+    <
+    MDBTextArea label = "Message"
+    id = "textAreaExample"
+    rows = {
+      4
+    }
+    /> < /
+    li > <
+    MDBBtn color = "info"
+    rounded className = "float-end" >
+    Send <
+    /MDBBtn> < /
+    MDBTypography > <
+    /MDBCol> < /
+    MDBRow > <
+    /MDBContainer>
+
   );
 }
