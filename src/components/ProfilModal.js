b0c923a7a4c5eac5ce09e0f222a3a7096548361f
JoannaRed <j.baranowska@hotmail.com>
2023-12-06 08:58:34 +0100

    added Profil Modal and New Chat button

---
+++ src/components/ProfilModal.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+export default function ProfileModal(props) {
+  return (
+    <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+
+        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+              <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                <div className="sm:flex sm:items-start">
+                  <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                    <svg className="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                    </svg>
+                  </div>
+                  <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                    <h3 className="text-base font-semibold leading-6 text-gray-900" id="modal-title">Deactivate account</h3>
+                    <div className="mt-2  flex justify-between" >
+                      <p className="text-sm text-gray-500">
+                        Joanna Baranowska
+                      </p>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"/></svg>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                <button 
+                onClick={() => props.setShow(false)}
+                type="button" className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">Deactivate</button>
+                <button 
+                  onClick={() => props.setShow(false)}
+                type="button" className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
+}
--- src/pages/NewHome.js
+++ src/pages/NewHome.js
@@ -1,10 +1,15 @@
 import UserCHatItem from '@/components/UserChatItem';
 import React from 'react';
+import ProfilModal from '@/components/ProfilModal';
 export default function NewHome() {
+
+    const showState = React.useState(false);
+    const show = showState[0];
+    const setShow = showState[1];
 
     return (
         <div className="container mx-auto shadow-lg rounded-lg">
-         
+
             <div className="px-5 py-5 flex justify-between items-center bg-white border-b-2">
                 <div className="font-semibold text-2xl">GoingChat</div>
                 <div className="w-1/2">
@@ -16,16 +21,23 @@ export default function NewHome() {
                         className="rounded-2xl bg-gray-100 py-3 px-5 w-full"
                     />
                 </div>
-                <div
-                    className="h-12 w-12 p-2 bg-yellow-500 rounded-full text-white font-semibold flex items-center justify-center">
-                    RA
-                </div>
+                <button
+                    onClick={() => setShow(!show)}
+                    className="h-12 w-12 p-2 bg-yellow-500 rounded-full text-white font-semibold flex items-center justify-center"
+                >
+                    Profil
+                </button>
             </div>
 
-            <div className="flex flex-row justify-between bg-white overflow-hidden" style={{height:'85vh'}}>
+            <div className="flex flex-row justify-between bg-white overflow-hidden" style={{ height: '85vh' }}>
                 <div className="flex flex-col w-2/5 border-r-2 overflow-y-auto">
                     <div className="border-b-2 py-4 px-2">
-                        <input type="text" placeholder="search chatting" className="py-2 px-2 border-2 border-gray-200 rounded-2xl w-full"/>
+                        <button type='button' className="flex border-b-2 py-4 px-2" onClick={() => console.log("Button clicked")}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                            </svg>
+                            <p className='ml-4'>New chat</p>
+                        </button>
                     </div>
                     <UserCHatItem name={'Luis1994'} avatar={"https://source.unsplash.com/_7LbC5J-jw4/600x600"} lastMessage={"Pick me at 9:00 Am"} />
                     <UserCHatItem name={'Luis1994'} avatar={"https://source.unsplash.com/_7LbC5J-jw4/600x600"} lastMessage={"Pick me at 9:00 Am"} />
@@ -37,7 +49,7 @@ export default function NewHome() {
                     <UserCHatItem name={'Luis1994'} avatar={"https://source.unsplash.com/_7LbC5J-jw4/600x600"} lastMessage={"Pick me at 9:00 Am"} />
                     <UserCHatItem name={'Luis1994'} avatar={"https://source.unsplash.com/_7LbC5J-jw4/600x600"} lastMessage={"Pick me at 9:00 Am"} />
                     <UserCHatItem name={'Luis1994'} avatar={"https://source.unsplash.com/_7LbC5J-jw4/600x600"} lastMessage={"Pick me at 9:00 Am"} />
-               
+
                 </div>
 
                 <div className="w-full px-5 flex flex-col justify-between">
@@ -112,22 +124,9 @@ export default function NewHome() {
                         />
                     </div>
                 </div>
-                <div className="w-2/5 border-l-2 px-5">
-                    <div className="flex flex-col">
-                        <div className="font-semibold text-xl py-4">Mern Stack Group</div>
-                        <img
-                            src="https://source.unsplash.com/L2cxSuKWbpo/600x600"
-                            className="object-cover rounded-xl h-64"
-                            alt=""
-                        />
-                        <div className="font-semibold py-4">Created 22 Sep 2021</div>
-                        <div className="font-light">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt,
-                            perspiciatis!
-                        </div>
-                    </div>
-                </div>
+
             </div>
+            {show && <ProfilModal setShow={setShow} />}
         </div>
     );
 }
