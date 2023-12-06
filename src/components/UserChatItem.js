07fd22d6dbb6b7de277f9a4f893bf12b6e56fa4f
JoannaRed <j.baranowska@hotmail.com>
2023-12-06 16:42:25 +0100

    onClick()

--- src/components/UserChatItem.js
+++ src/components/UserChatItem.js
@@ -1,19 +1,19 @@
-export default function UserCHatItem({name,lastMessage,avatar}){
+export default function UserCHatItem({onClick, name,lastMessage,avatar}){
 
 
     return (
-        <div className="flex flex-row py-4 px-2 justify-center items-center border-b-2">
-        <div className="w-1/4">
-            <img
-                src={avatar}
-                className="object-cover h-12 w-12 rounded-full"
-                alt=""
-            />
-        </div>
-        <div className="w-full">
-            <div className="text-lg font-semibold">{name}</div>
-            <span className="text-gray-500">{lastMessage}</span>
-        </div>
+        <div onClick={onClick} className="flex flex-row py-4 px-2 justify-center items-center border-b-2">
+            <div className="w-1/4">
+                <img
+                    src={avatar}
+                    className="object-cover h-12 w-12 rounded-full"
+                    alt=""
+                />
+            </div>
+            <div className="w-full">
+                <div className="text-lg font-semibold">{name}</div>
+                <span className="text-gray-500">{lastMessage}</span>
+            </div>
     </div>
     )
 }
--- src/pages/NewHome.js
+++ src/pages/NewHome.js
@@ -1,17 +1,22 @@
 import UserCHatItem from '@/components/UserChatItem';
 import React from 'react';
 import ProfilModal from '@/components/ProfilModal';
+import { NoChatSelected } from '@/components/NoChatSelected';
 export default function NewHome() {
 
     const showState = React.useState(false);
     const show = showState[0];
     const setShow = showState[1];
+    const [showMessages, setShowMessages] = React.useState(false);
 
     return (
         <div className="container mx-auto shadow-lg rounded-lg">
-
             <div className="px-5 py-5 flex justify-between items-center bg-white border-b-2">
-                <div className="font-semibold text-2xl">GoingChat</div>
+                <div className="font-semibold text-2xl">
+                    <button type="button" onClick={() => setShowMessages(false)}   >
+                        Accueil
+                    </button>
+                </div>
                 <div className="w-1/2">
                     <input
                         type="text"
@@ -28,7 +33,6 @@ export default function NewHome() {
                     Profil
                 </button>
             </div>
-
             <div className="flex flex-row justify-between bg-white overflow-hidden" style={{ height: '85vh' }}>
                 <div className="flex flex-col w-2/5 border-r-2 overflow-y-auto">
                     <div className="border-b-2 py-4 px-2">
@@ -39,92 +43,86 @@ export default function NewHome() {
                             <p className='ml-4'>New chat</p>
                         </button>
                     </div>
-                    <UserCHatItem name={'Luis1994'} avatar={"https://source.unsplash.com/_7LbC5J-jw4/600x600"} lastMessage={"Pick me at 9:00 Am"} />
-                    <UserCHatItem name={'Luis1994'} avatar={"https://source.unsplash.com/_7LbC5J-jw4/600x600"} lastMessage={"Pick me at 9:00 Am"} />
-                    <UserCHatItem name={'Luis1994'} avatar={"https://source.unsplash.com/_7LbC5J-jw4/600x600"} lastMessage={"Pick me at 9:00 Am"} />
-                    <UserCHatItem name={'Luis1994'} avatar={"https://source.unsplash.com/_7LbC5J-jw4/600x600"} lastMessage={"Pick me at 9:00 Am"} />
-                    <UserCHatItem name={'Luis1994'} avatar={"https://source.unsplash.com/_7LbC5J-jw4/600x600"} lastMessage={"Pick me at 9:00 Am"} />
-                    <UserCHatItem name={'Luis1994'} avatar={"https://source.unsplash.com/_7LbC5J-jw4/600x600"} lastMessage={"Pick me at 9:00 Am"} />
-                    <UserCHatItem name={'Luis1994'} avatar={"https://source.unsplash.com/_7LbC5J-jw4/600x600"} lastMessage={"Pick me at 9:00 Am"} />
-                    <UserCHatItem name={'Luis1994'} avatar={"https://source.unsplash.com/_7LbC5J-jw4/600x600"} lastMessage={"Pick me at 9:00 Am"} />
-                    <UserCHatItem name={'Luis1994'} avatar={"https://source.unsplash.com/_7LbC5J-jw4/600x600"} lastMessage={"Pick me at 9:00 Am"} />
-                    <UserCHatItem name={'Luis1994'} avatar={"https://source.unsplash.com/_7LbC5J-jw4/600x600"} lastMessage={"Pick me at 9:00 Am"} />
-
+                    <UserCHatItem name={'Luis1994'} avatar={"https://source.unsplash.com/_7LbC5J-jw4/600x600"} lastMessage={"Pick me at 9:00 Am"} onClick={() => setShowMessages(true)} />
+                    <UserCHatItem name={'Luis1994'} avatar={"https://source.unsplash.com/_7LbC5J-jw4/600x600"} lastMessage={"Pick me at 9:00 Am"} onClick={() => setShowMessages(true)}/>
+                    <UserCHatItem name={'Luis1994'} avatar={"https://source.unsplash.com/_7LbC5J-jw4/600x600"} lastMessage={"Pick me at 9:00 Am"} onClick={() => setShowMessages(true)}/>
+                    <UserCHatItem name={'Luis1994'} avatar={"https://source.unsplash.com/_7LbC5J-jw4/600x600"} lastMessage={"Pick me at 9:00 Am"} onClick={() => setShowMessages(true)}/>
+                    <UserCHatItem name={'Luis1994'} avatar={"https://source.unsplash.com/_7LbC5J-jw4/600x600"} lastMessage={"Pick me at 9:00 Am"} onClick={() => setShowMessages(true)}/>
                 </div>
-
-                <div className="w-full px-5 flex flex-col justify-between">
-                    <div className="flex flex-col mt-5">
-                        <div className="flex justify-end mb-4">
-                            <div
-                                className="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white"
-                            >
-                                Welcome to group everyone !
-                            </div>
-                            <img
-                                src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-                                className="object-cover h-8 w-8 rounded-full"
-                                alt=""
-                            />
-                        </div>
-                        <div className="flex justify-start mb-4">
-                            <img
-                                src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-                                className="object-cover h-8 w-8 rounded-full"
-                                alt=""
-                            />
-                            <div
-                                className="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white"
-                            >
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
-                                at praesentium, aut ullam delectus odio error sit rem. Architecto
-                                nulla doloribus laborum illo rem enim dolor odio saepe,
-                                consequatur quas?
-                            </div>
-                        </div>
-                        <div className="flex justify-end mb-4">
-                            <div>
+                {showMessages && (
+                    <div className="w-full px-5 flex flex-col justify-between">
+                        <div className="flex flex-col mt-5">
+                            <div className="flex justify-end mb-4">
                                 <div
                                     className="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white"
                                 >
-                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                                    Magnam, repudiandae.
+                                    Welcome to group everyone !
                                 </div>
-
+                                <img
+                                    src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
+                                    className="object-cover h-8 w-8 rounded-full"
+                                    alt=""
+                                />
+                            </div>
+                            <div className="flex justify-start mb-4">
+                                <img
+                                    src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
+                                    className="object-cover h-8 w-8 rounded-full"
+                                    alt=""
+                                />
                                 <div
-                                    className="mt-4 mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white"
+                                    className="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white"
                                 >
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                    Debitis, reiciendis!
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+                                    at praesentium, aut ullam delectus odio error sit rem. Architecto
+                                    nulla doloribus laborum illo rem enim dolor odio saepe,
+                                    consequatur quas?
                                 </div>
                             </div>
-                            <img
-                                src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-                                className="object-cover h-8 w-8 rounded-full"
-                                alt=""
-                            />
-                        </div>
-                        <div className="flex justify-start mb-4">
-                            <img
-                                src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-                                className="object-cover h-8 w-8 rounded-full"
-                                alt=""
-                            />
-                            <div
-                                className="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white"
-                            >
-                                happy holiday guys!
+                            <div className="flex justify-end mb-4">
+                                <div>
+                                    <div
+                                        className="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white"
+                                    >
+                                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                                        Magnam, repudiandae.
+                                    </div>
+
+                                    <div
+                                        className="mt-4 mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white"
+                                    >
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                        Debitis, reiciendis!
+                                    </div>
+                                </div>
+                                <img
+                                    src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
+                                    className="object-cover h-8 w-8 rounded-full"
+                                    alt=""
+                                />
+                            </div>
+                            <div className="flex justify-start mb-4">
+                                <img
+                                    src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
+                                    className="object-cover h-8 w-8 rounded-full"
+                                    alt=""
+                                />
+                                <div
+                                    className="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white"
+                                >
+                                    happy holiday guys!
+                                </div>
                             </div>
                         </div>
+                        <div className="py-5">
+                            <input
+                                className="w-full bg-gray-300 py-5 px-3 rounded-xl"
+                                type="text"
+                                placeholder="type your message here..."
+                            />
+                        </div>
                     </div>
-                    <div className="py-5">
-                        <input
-                            className="w-full bg-gray-300 py-5 px-3 rounded-xl"
-                            type="text"
-                            placeholder="type your message here..."
-                        />
-                    </div>
-                </div>
-
+                )}
             </div>
             {show && <ProfilModal setShow={setShow} />}
         </div>
